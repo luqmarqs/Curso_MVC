af2454cfa752ead8cb4b2d066064ec8c4b556b3b
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-05-21 22:19:01 -0300

    Adicionar arquivos de projeto.

---
+++ Curso_MVC/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso_MVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"data source=LUCASMARQUES\SQLEXPRESS;initial catalog=master;trusted_connection=true");
+        }
+    }
+}
